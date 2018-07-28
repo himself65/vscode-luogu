e2c69ff11712fbdb79dceef66bf254020c23fcd7
Himself65 <himself6565@gmail.com>
2018-07-28 17:13:25 +0800

    update

--- src/commands/search.ts
+++ src/commands/search.ts
@@ -10,18 +10,21 @@ export async function searchProblem(channel: vscode.OutputChannel, uri?: vscode.
     if (!input) { return; }
 
     await getProblem(input, problem => {
-        let pannel = vscode.window.createWebviewPanel(problem.getStringPID(), problem.getName(), vscode.ViewColumn.One);
+        let pannel = vscode.window.createWebviewPanel(problem.getStringPID(), problem.getName(), vscode.ViewColumn.Two);
         let content = md.render(problem.toMarkDown());
-        pannel.webview.html = ` <!DOCTYPE html>
+        let html = `<!DOCTYPE html>
         <html lang="en">
         <head>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>${problem.getName()}</title>
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@9.12.0/build/styles/default.min.css">
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.9.0/katex.min.css">
         </head>
         <body>
         ${content}
         </body>
         </html>`;
+        pannel.webview.html = html;
     });
 }
--- src/extension.ts
+++ src/extension.ts
@@ -12,15 +12,19 @@ export function activate(context: vscode.ExtensionContext) {
         vscode.window.showInformationMessage('Developed by Himself65');
     };
 
+    let notice = function () {
+        vscode.window.showInformationMessage('功能未完成，敬请期待');
+    };
+
     context.subscriptions.push(
         // vscode.window.registerTreeDataProvider("luoguExplorer", luoguTreeDataProvider),
         vscode.commands.registerCommand("luogu.about", about),
-        vscode.commands.registerCommand("luogu.signin", () => luoguManager.signIn(channel)),
-        vscode.commands.registerCommand("luogu.signout", () => luoguManager.signOut(channel)),
-        vscode.commands.registerCommand("luogu.showProblem", () => { }),
+        vscode.commands.registerCommand("luogu.signin", () => notice),
+        vscode.commands.registerCommand("luogu.signout", () => notice),
+        vscode.commands.registerCommand("luogu.showProblem", () => notice),
         vscode.commands.registerCommand("luogu.searchProblem", () => searchProblem(channel)),
-        vscode.commands.registerCommand("luogu.submitSolution", () => { }),
-        vscode.commands.registerCommand("luogu.refreshExplorer", () => { })
+        vscode.commands.registerCommand("luogu.submitSolution", () => notice),
+        vscode.commands.registerCommand("luogu.refreshExplorer", () => notice)
     );
 
     luoguManager.on('statusChanged', () => {
