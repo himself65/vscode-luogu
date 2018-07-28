73961c4abcea467f0655c7d4c9076c2d85be177c
Himself65 <himself6565@gmail.com>
2018-07-28 16:09:38 +0800

    fix bugs

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,13 +1,14 @@
 import * as axois from 'axios';
 import { Problem } from '../data/Problem';
 
-export async function getProblem(id: string, callback: (data: Problem) => void): Promise<void> {
+export async function getProblem(id: string, callback: (data: Problem) => void, fail?: (message: string) => void): Promise<void> {
     axois.default.get(`https://www.luogu.org/api/problem/detail/${id}`).then(res => {
-        console.log(`得到题目：${id}`);
         if (res.data.status === 200) {
+            console.log(`得到题目：${id}`);
             callback(new Problem(res.data.data));
         } else {
-
+            const err = '找不到题目';
+            fail ? fail(err) : console.error(err);
         }
     });
 }
--- src/utils/markdown.ts
+++ src/utils/markdown.ts
@@ -2,4 +2,4 @@ import * as markdownit from 'markdown-it';
 import * as markdownitkatex from '@luogu-dev/markdown-it-katex';
 import * as markdownhighlight from 'markdown-it-highlightjs';
 
-export const md = new markdownit().use(markdownitkatex).use(markdownitkatex);
+export const md = new markdownit().use(markdownitkatex).use(markdownhighlight);
