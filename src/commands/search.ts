31880f3dd6c2251bf6746827b69f2d9b6cba84e8
Himself65 <himself6565@gmail.com>
2018-07-28 15:30:53 +0800

    update

--- src/commands/search.ts
+++ src/commands/search.ts
@@ -2,7 +2,7 @@
 
 import * as vscode from 'vscode';
 import { getProblem } from '../utils/api';
-import * as markdownit from 'markdown-it';
+import { md } from '../utils/markdown';
 
 export async function searchProblem(channel: vscode.OutputChannel, uri?: vscode.Uri): Promise<void> {
     let input = await vscode.window.showInputBox({ placeHolder: '输入题号' });
@@ -11,10 +11,7 @@ export async function searchProblem(channel: vscode.OutputChannel, uri?: vscode.
 
     await getProblem(input, problem => {
         let pannel = vscode.window.createWebviewPanel(problem.getStringPID(), problem.getName(), vscode.ViewColumn.One);
-        // new vscode.MarkdownString(problem.toMarkDown());
-        let md = new markdownit("default");
-        let str = md.render(problem.toMarkDown());
-        console.log(str);
+        let content = md.render(problem.toMarkDown());
         pannel.webview.html = ` <!DOCTYPE html>
         <html lang="en">
         <head>
@@ -23,7 +20,7 @@ export async function searchProblem(channel: vscode.OutputChannel, uri?: vscode.
             <title>${problem.getName()}</title>
         </head>
         <body>
-        ${str}
+        ${content}
         </body>
         </html>`;
     });
---
+++ src/utils/markdown.ts
@@ -0,0 +1,5 @@
+import * as markdownit from 'markdown-it';
+import * as markdownitkatex from '@luogu-dev/markdown-it-katex';
+import * as markdownhighlight from 'markdown-it-highlightjs';
+
+export const md = new markdownit().use(markdownitkatex).use(markdownitkatex);
